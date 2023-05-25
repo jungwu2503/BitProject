d1a50cfa9123850c7a85ca3cdeeabe9f7fc95085
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2023-05-25 19:38:23 +0900

    Update GisaQuiz.java

--- DAO를 통한 SQL 처리/GisaQuiz.java
+++ DAO를 통한 SQL 처리/GisaQuiz.java
@@ -11,7 +11,9 @@ public class GisaQuiz {
 	public String solveQuiz1() {
 		String answer = null;
 		// 로직 처리(SQL)
-		/*
+		/*지역코드가 B 인 자료에 대하여 (국어점수 + 영어점수)으로 내림차순 정렬했을 
+		때 5번째 학번 출력하시오. 동일값 발생시 학번에 대한 오름차순 정렬하시오.(20점) (※ 결
+		과 값은 수검자 PC의 C:\C_it\data\Ans1.txt 파일에 출력되도록 프로그램을 작성할 것)
 		 *  select std_no
 			from gisaTBL
 			where loc_code = 'B' order by (kor+eng) desc, std_no asc limit 4, 1;
